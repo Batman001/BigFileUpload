4e1d596d9ae30642fbbe5f14a4a0daa79898c49c
Batman007 <sun_thinker@163.com>
2019-07-09 10:10:49 +0800

    （1)添加前后端跨域问题的解决类（全局） 使用配置类实现全局配置 （2）修改file_info的表结构 使用identifier作为主键

---
+++ src/main/java/com/test/fileupload/config/CorsConfigure.java
@@ -0,0 +1,20 @@
+package com.test.fileupload.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author Batman create on 2019-07-09 00:06
+ * 解决跨域问题的配置类
+ */
+@Configuration
+public class CorsConfigure extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
