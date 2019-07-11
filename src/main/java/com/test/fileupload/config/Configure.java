9e92534e9d261c2d2a77fc1cf329d6c323af6758
Batman007 <sun_thinker@163.com>
2019-07-11 10:08:11 +0800

    （1)添加前后端跨域问题的解决类（全局） 使用配置类实现全局配置 （2)解决文件分片的最后一个分片有可能大于chunkSize的情况 通过配置类添加文件分片大小的上限

--- src/main/java/com/test/fileupload/config/Configure.java
+++ src/main/java/com/test/fileupload/config/Configure.java
@@ -1,20 +1,34 @@
 package com.test.fileupload.config;
 
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * @author Batman create on 2019-07-09 00:06
  * 解决跨域问题的配置类
  */
 @Configuration
-public class CorsConfigure extends WebMvcConfigurerAdapter {
+public class Configure extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
                 .allowCredentials(true).maxAge(3600);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // 单个数据的大小 设置2Mb
+        factory.setMaxFileSize("2048KB");
+        // 总上传数据的大小 设置为1024Mb
+        factory.setMaxRequestSize("1048576KB");
+        return factory.createMultipartConfig();
     }
 }
